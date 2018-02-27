00cef08cd583d709fff734eca749d6464504f308
user <user@172.31.54.93>
2018-02-27 18:52:49 +0300

    создал проект

---
+++ src/lesson1/my_prog_1.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+public class my_prog_1 {
+
+	static public void main(String[] args)
+	{
+		String s = "Моя первая программа";
+		int a = 5;
+		double d = 3.34561;
+		boolean b = true;
+		boolean b2 = false;
+		
+		System.out.println(s + " " + a);
+		System.out.printf("%s: %d вывод %f", s, a, d);
+
+	}
+}
